985b6e5f2971d967b1ce2e4068a5c7f903708997
Your Name <shrivasvinay@123gmail.com>
2026-08-09 23:12:43 +0530

    my code

--- vinay.py
+++ vinay.py
@@ -23,10 +23,11 @@ if a%2==0:
 else:
     print("aria of rectangle",a*c)
 '''
-math=int(input("enter first number"))
-hindi=int(input("enter 2nd number"))
-english=int(input("enter 3rd number"))
-total=math+hindi+english
-parsent=total*100/300
-print(parsent,"%")
+a=int(input("enter a number"))
+'''
+'''
+if a%5==0:
+    print("number is divisible by 5")
+else:
+    print("number is not divisible by 5")
 
